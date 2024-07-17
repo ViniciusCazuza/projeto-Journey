49a453e89e69428712b1dfa5f9f1e9eae6ad2cbf
Vinicius Cazuza <cazuza.dev@hotmail.com>
2024-07-16 21:57:11 -0300

    Iniciando o projeto

---
+++ src/Journey.Api/Controllers/TripsController.cs
@@ -0,0 +1,21 @@
+using Journey.Communication.Requests;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Journey.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class TripsController : ControllerBase
+
+{
+    [HttpPost]
+    public IActionResult Register([FromBody] RequestRegisterTripJson request)
+    {
+
+
+        return Created();
+
+
+    }
+}
